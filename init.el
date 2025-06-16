816ac20295ad7bd75ad4afae40957f8b50f6abd3
JolliestJames <cloudismydog@gmail.com>
2025-06-16 11:13:40 -0700

    update init.el

--- init.el
+++ init.el
@@ -2,11 +2,13 @@
 
 (add-to-list 'default-frame-alist '(font . "Fira Mono-20"))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(toggle-frame-fullscreen)
 
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode -1)
+(fido-mode 1)
 
 ;; Set up package.el to work with MELPA
 (require 'package)
@@ -25,11 +27,12 @@
 (evil-set-undo-system 'undo-redo)
 
 ;; Load Everforest
-(add-to-list 'custom-theme-load-path "~/.emacs.d/everforest-theme")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/everforest-emacs")
 (load-theme 'everforest-hard-dark t)
 
 ;; Use vterm
-(use-package vterm
-    :ensure t)
+(if (not (eq system-type 'windows-nt))
+    (use-package vterm
+	:ensure t))
 
 (load-file custom-file)
